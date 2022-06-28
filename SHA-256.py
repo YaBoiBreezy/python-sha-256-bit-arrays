e11e52b580608976a71a151fc5cc90cb8350a28f
YaBoiBreezy <91281967+YaBoiBreezy@users.noreply.github.com>
2022-06-28 11:33:35 -0400

    made function defs

--- SHA-256.py
+++ SHA-256.py
@@ -1,6 +1,25 @@
 #this is the main file
 
-
+#main function, takes array of size <= 512 of numbers 0 <= num <= 1
+def generate_hash(input):
+    
+#basic rotation
+def rightRotate(arr,dist):
+    
+#shift over, leave 0's in wake
+def rightShift(arr,dist):
+    
+#invert every value
+def noter(arr,dist):
+    
+#apply xor to every pair of bits
+def xor(arr1,arr2):
+    
+#apply and to every pair of bits
+def ander(arr1,arr2):
+    
+#binary addition, discard overflow
+def plus(arr1,arr2):
 
 if __name__ == "__main__":
     print(generate_hash("hello").hex())
