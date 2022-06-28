b836e7f327d761d0c67a22c5abdffdaa52564fca
YaBoiBreezy <91281967+YaBoiBreezy@users.noreply.github.com>
2022-06-28 11:50:15 -0400

    added all, fixed some

--- SHA-256.py
+++ SHA-256.py
@@ -5,30 +5,53 @@ def generate_hash(input):
     
 #basic rotation
 def rightRotate(arr,dist):
+    ret=[]
     for x in range (32):
-        hold=arr[(x+dist)%32]
-        arr[(x+dist)%32]=arr[x%32]
-        arr[x%32]=hold
+        ret.append(arr[(x+dist)%32])
+    return ret
     
 #shift over, leave 0's in wake
 def rightShift(arr,dist):
-    for x in range (31,dist,-1):
-        arr[x]=arr[x-dist]
-    for x in range (dist):
-        arr[x]=0
+    ret=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for x in range (dist,32):
+        ret[x]=arr[x-dist]
+    return ret
     
 #invert every value
 def noter(arr,dist):
+    ret=[]
+    for x in range (32):
+        ret.append(1-arr[x])
+    return ret
     
 #apply xor to every pair of bits
 def xor(arr1,arr2):
+    ret=[]
+    for x in range (32):
+        ret.append(1 if arr1[x]==arr2[x] else 0)
+    return ret
     
 #apply and to every pair of bits
 def ander(arr1,arr2):
+    ret=[]
+    for x in range (32):
+        ret.append(1 if arr1[x] or arr2[x] else 0)
+    return ret
     
 #binary addition, discard overflow
 def plus(arr1,arr2):
+    ret=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    carry=0
+    for x in range(31, -1, -1):
+        r = carry+arr1[x]+arr2[x]
+        ret[x] = (1 if r % 2 == 1 else 0)
+        carry = 0 if r < 2 else 1
+    return ret
 
 if __name__ == "__main__":
     print(rightRotate([0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,1],2))
     print(rightShift([1,1,1,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,1],2))
+    print(noter([1,1,1,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,1],2))
+    print(xor([1,1,1,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,1],2))
+    print(ander([1,1,1,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,1],2))
+    print(plus([1,1,1,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,1],2))
